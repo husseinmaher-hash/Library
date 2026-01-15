8654e96eec48f38abd80b6f3a42775d165318de6
husseinmaher-hash <hussein.maher@optimumpartners.co>
2026-01-15 15:15:18 +0300

    fix some files

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class AppConfig:
-    secretKey = os.getenv("secretKey")
-    databaseUrl = os.getenv("databaseUrl")
+    secretKey = os.getenv("SECRET_KEY")
+    databaseUrl = os.getenv("DATABASE_URL")
     sqlalchemyTrackModifications = False
